c1165697e32b885ea3d1adb1b68352179335e37d
Michael Heyde <michael.heyde@tu-dresden.de>
2015-08-17 20:07:33 +0200

    added proto panel

--- src/main/java/bdv/jogl/VolumeRenderer/gui/TransferFunctionDataPanel.java
+++ src/main/java/bdv/jogl/VolumeRenderer/gui/TransferFunctionDataPanel.java
@@ -1,6 +1,12 @@
 package bdv.jogl.VolumeRenderer.gui;
 
+import java.awt.Color;
+import java.awt.Point;
+
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.text.TableView;
 
 /**
  * direct data manipulation panel
@@ -8,5 +14,19 @@ import javax.swing.JPanel;
  *
  */
 public class TransferFunctionDataPanel extends JPanel {
-
+	
+	private JTable pointTable = new JTable(new Object[][]{{new Point(0,0)}}, new String[]{"Transfer function points"}); 
+	
+	private JTable colorTable = new JTable(new Object[][]{{new Point(0,0),Color.red}}, new String[]{"Positions","Color"});
+	
+	private BoxLayout mainLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+	
+	public TransferFunctionDataPanel(){
+		
+		setLayout(mainLayout);
+		
+		add(pointTable);
+		
+		add(colorTable);
+	}
 }
