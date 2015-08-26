fa49dd993320303b34f2ae7b59df33fa0027903c
Michael Heyde <michael.heyde@tu-dresden.de>
2015-08-26 14:40:27 +0200

    cleanup avg

--- src/main/java/bdv/jogl/VolumeRenderer/ShaderPrograms/ShaderSources/funtions/AverageVolumeAccumulator.java
+++ src/main/java/bdv/jogl/VolumeRenderer/ShaderPrograms/ShaderSources/funtions/AverageVolumeAccumulator.java
@@ -7,27 +7,16 @@ public class AverageVolumeAccumulator extends AbstractVolumeAccumulator {
 	@Override
 	public String[] declaration() {
 		String[] dec= new String[]{
-		"#line "+Thread.currentThread().getStackTrace()[1].getLineNumber(),
-		"float "+getFunctionName()+"(float densities["+scvMaxNumberOfVolumes+"]) {",
-		"	float density = 0;",	
-		"	int intersectingVolumes = 0;",	
-		"	for(int n = 0; n < "+scvMaxNumberOfVolumes+"; n++){",
-	/*	"		if(any(lessThan(ray_poss[n], zeros))){",
-		"			continue;",
-		"		}",
-		"		if(any(greaterThan(ray_poss[n], ones))){",
-		"			continue;",
-		"		}",			*/
-		"		density += densities[n]-"+suvMinVolumeValue+";",
-		"		intersectingVolumes++;",
-		"	}",
-		"	if(intersectingVolumes==0){",
-		"		intersectingVolumes=1;",
-		"	}",
-		"	density/=intersectingVolumes;",
-		"	return density;",	
-		"}"
-			};
+				"#line "+Thread.currentThread().getStackTrace()[1].getLineNumber()+ " 1",
+				"float "+getFunctionName()+"(float densities["+scvMaxNumberOfVolumes+"]) {",
+				"	float density = 0;",		
+				"	for(int n = 0; n < "+scvMaxNumberOfVolumes+"; n++){",
+				"		density += densities[n]-"+suvMinVolumeValue+";",
+				"	}",
+				"	density/="+scvMaxNumberOfVolumes+";",
+				"	return density;",	
+				"}"
+		};
 		return dec;
 	}
 
