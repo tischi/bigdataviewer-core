90f2b80db68174d076faf35a88c5e2f1a44ba4e6
Michael Heyde <michael.heyde@tu-dresden.de>
2015-09-03 12:07:11 +0200

    refactorings

--- src/main/java/bdv/jogl/VolumeRenderer/TransferFunctions/sampler/RegularSampler.java
+++ src/main/java/bdv/jogl/VolumeRenderer/TransferFunctions/sampler/RegularSampler.java
@@ -38,29 +38,29 @@ public class RegularSampler implements ITransferFunctionSampler {
 				continue;
 			}
 
-			float[] currentColor = getNormalizedColor(colorMap.get(latestMapIndex));
+			float[] startColor = getNormalizedColor(colorMap.get(latestMapIndex));
 			float[] finalColor = getNormalizedColor(colorMap.get(currentMapIndex));
+			float[] currentColor = startColor.clone();
 			float[] colorGradient = {0,0,0,0};
 			
 			//forward difference
 			for(int dim = 0; dim < colorGradient.length; dim++){
-				colorGradient[dim] = (finalColor[dim]-currentColor[dim])/(currentMapIndex-latestMapIndex);
+				colorGradient[dim] = (finalColor[dim]-startColor[dim])/((float)(currentMapIndex-latestMapIndex));
 			}
 
 			//sample linear
 			for(Integer step = latestMapIndex; step < currentMapIndex; step++ ){
-
+				buffer.put(currentColor.clone());
+				
 				//add to buffer and increment
-				for(int dim = 0; dim < colorGradient.length; dim++){
-					buffer.put(Math.min( finalColor[dim],  currentColor[dim]));
+				for(int dim = 0; dim < colorGradient.length; dim++){	
 					currentColor[dim] += colorGradient[dim];
+					currentColor[dim] = Math.min(1.f, Math.max(0.f, currentColor[dim]));
 				}
 			}		
 			//add latest color
 			if(currentMapIndex == colorMap.lastKey()){
-				for(int dim = 0; dim < finalColor.length; dim++){
-					buffer.put(finalColor[dim]);
-				}
+				buffer.put(finalColor.clone());
 			}
 			latestMapIndex = currentMapIndex;
 		}
