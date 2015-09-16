6cab641f43b3cc2b3bca364b54580d4c3cfb2c84
Michael Heyde <michael.heyde@tu-dresden.de>
2015-09-16 08:50:30 +0200

    corrections and max for volume

--- src/main/java/bdv/jogl/VolumeRenderer/ShaderPrograms/ShaderSources/functions/VolumeGradientEvaluationFunction.java
+++ src/main/java/bdv/jogl/VolumeRenderer/ShaderPrograms/ShaderSources/functions/VolumeGradientEvaluationFunction.java
@@ -17,7 +17,7 @@ public class VolumeGradientEvaluationFunction extends AbstractShaderFunction {
 				"	vec3 down = texCoord+vec3(0.0,-"+scvMinDelta+",0.0);",
 				"	vec3 front = texCoord+vec3(0.0,0.0,-"+scvMinDelta+");",
 				"	vec3 back = texCoord+vec3(0.0,0.0,"+scvMinDelta+");",*/
-				"	const float offset = 0.1;",
+				"	const float offset = 0.001;",
 				"	float center = texture(volume,texCoord);",
 				"	vec3 plus = vec3(	texture(volume,texCoord+vec3(offset,0.0,0.0)).r,",
 				"						texture(volume,texCoord+vec3(0.0,offset,0.0)).r,",
@@ -26,7 +26,7 @@ public class VolumeGradientEvaluationFunction extends AbstractShaderFunction {
 				"						texture(volume,texCoord+vec3(0.0,-offset,0.0)).r,",
 				"						texture(volume,texCoord+vec3(0.0,0.0,-offset)).r);",
 				"	vec3 factor = vec3(0.5);",
-			/*	"	for(int d =0; d < 3; d++){",
+				"	for(int d =0; d < 3; d++){",
 				"		if(plus[d] < 0.0){",
 				"			plus[d] = center;",
 				"			factor[d] = 1.0;",
@@ -35,7 +35,7 @@ public class VolumeGradientEvaluationFunction extends AbstractShaderFunction {
 				"			minus[d] = center;",
 				"			factor[d] = 1.0;",
 				"		}",
-				"	}",*/
+				"	}",
 				"",
 				"   vec3 gradient = factor * (plus- minus);",
 				"	return gradient;",
