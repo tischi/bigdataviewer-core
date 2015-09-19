92523768d6e61850046b9e714d6ba1d5e5c5692b
Michael Heyde <michael.heyde@tu-dresden.de>
2015-09-19 09:56:40 +0200

    fixed aabb selection

--- src/main/java/bdv/jogl/VolumeRenderer/BigDataViewerDataSelector.java
+++ src/main/java/bdv/jogl/VolumeRenderer/BigDataViewerDataSelector.java
@@ -12,30 +12,32 @@ import com.jogamp.opengl.math.geom.AABBox;
 
 import bdv.BigDataViewer;
 import static bdv.jogl.VolumeRenderer.utils.MatrixUtils.*;
+import static bdv.jogl.VolumeRenderer.utils.VolumeDataUtils.*;
+import bdv.jogl.VolumeRenderer.utils.VolumeDataBlock;
 import bdv.viewer.Source;
 import bdv.viewer.state.SourceState;
 
 public class BigDataViewerDataSelector {
-	
+
 	private final BigDataViewer bdv;
-	
+
 	public BigDataViewerDataSelector(final BigDataViewer bdv){
 		this.bdv = bdv;
-		
+
 		initListener();
 	}
-	
+
 	/**
 	 * init all needed listeners
 	 */
 	private synchronized void initListener() {
-	
+
 		this.bdv.getViewerFrame().getViewerPanel().addMouseListener(new MouseAdapter() {
-			
+
 			@Override
 			public synchronized void mouseClicked(MouseEvent e) {
 				System.out.println("Hallo");
-				
+
 				//select on double click
 				if(e.getClickCount() == 2){
 					selectVolumePart(e.getPoint());
@@ -43,20 +45,31 @@ public class BigDataViewerDataSelector {
 			}
 		});
 	}
-	
+
 	/**
 	 * event to select a appropriate part of the volumes
 	 * @param p point on the panel
 	 */
 	public void selectVolumePart(final Point p){
-		AABBox volumeRectangle = getVolumeRegion(bdv, p, new float[]{10,10,10});
+		List<SourceState<?>> sources = bdv.getViewer().getState().getSources();
 		
-		AABBox[] volumeCoords = getInnerVolumes(bdv, volumeRectangle, 0, bdv.getViewer().getState().getCurrentTimepoint());
-		for(AABBox b : volumeCoords){
+		AABBox volumeRectangle = getVolumeRegion(bdv, p, new float[]{10,10,10});
+
+		
+		//TODO remove test
+		
+		for(int i =0; i < sources.size(); i++){
+			int midmapLevel = bdv.getViewer().getState().getSources().get(i).getSpimSource().getNumMipmapLevels()-1;
+			AABBox b = getInnerVolume(bdv, volumeRectangle, midmapLevel, bdv.getViewer().getState().getCurrentTimepoint(),i);
+
 			System.out.println(b);
+			VolumeDataBlock data = getDataBlock(bdv, b, i, midmapLevel);
+
+			System.out.println(data);
+			i++;
 		}
 	}
-	
+
 	/**
 	 * get the AABB of the queried region
 	 * @param bdv data source
@@ -68,74 +81,72 @@ public class BigDataViewerDataSelector {
 		AffineTransform3D transform3D = new AffineTransform3D();
 		bdv.getViewer().getState().getViewerTransform(transform3D);
 		Matrix4 transformJogl = convertToJoglTransform(transform3D);
+		transformJogl.invert();
+		
 		
 		//get coord in global space
 		float transformer[] = new float[]{queryPoint.x,queryPoint.y,0,1};
 		float transformed[] = new float[4];
 		transformJogl.multVec(transformer, transformed);
-		
+
 		//wclip
 		for(int i = 0; i < 3; i++ ){
 			transformed[i]/=transformed[3];
 		}
-	
+
 		//build box
 		AABBox box = new AABBox(transformed[0]-dimensions[0],transformed[1]-dimensions[1],transformed[2]-dimensions[2],
 				transformed[0]+dimensions[0],transformed[1]+dimensions[1],transformed[2]+dimensions[2]);
 		return box;
 	}
-	
+
 	/**
 	 * calculates the inner Volume borders of a certain region 
 	 * @param bdv viewer
 	 * @param outerVolume outer region
 	 * @param midmapLevel current midmap to query
 	 * @param time current time to query
+	 * @param sorceId it of the source to query
 	 * @return AABB of the volumes in volume coordinate system
 	 */
-	private static AABBox[] getInnerVolumes(final BigDataViewer bdv, final AABBox outerVolume, int midmapLevel, int time){
+	private static AABBox getInnerVolume(final BigDataViewer bdv, final AABBox outerVolume, int midmapLevel, int time, int sourceId){
 		//iterate sources
 		List<SourceState<?>> sources = bdv.getViewer().getState().getSources();
-		AABBox results[] = new AABBox[sources.size()];
+
+		Source<?> source = sources.get(sourceId).getSpimSource();
+
+		//get volume trans  
+		AffineTransform3D sourceTrans3D = new AffineTransform3D();
+		source.getSourceTransform(time, midmapLevel, sourceTrans3D);
+		Matrix4 sourceTransJogl = convertToJoglTransform(sourceTrans3D);
+		sourceTransJogl.invert();
 		
-		for(int n = 0; n < sources.size(); n++){
-			Source<?> source = sources.get(n).getSpimSource();
-			
-			//get volume trans  
-			AffineTransform3D sourceTrans3D = new AffineTransform3D();
-			source.getSourceTransform(time, midmapLevel, sourceTrans3D);
-			Matrix4 sourceTransJogl = convertToJoglTransform(sourceTrans3D);
-			
-			//test points for borders since they may overlap
-			float[][]minMax={{Float.MAX_VALUE,Float.MAX_VALUE,Float.MAX_VALUE},
-					{Float.MIN_VALUE,Float.MIN_VALUE,Float.MIN_VALUE}};
-			float z = outerVolume.getMinZ();
-			for(int i= 0; i < 2; i++ ){
-				float y = outerVolume.getMinY();
-				for(int j =0; j < 2; j++){
-					float x =  outerVolume.getMinX();
-					for(int k =0; k < 2; k++){
-						float transformer[]={x,y,z,1};
-						float transformed[] = new float[4];
-						
-						sourceTransJogl.multVec(transformer,transformed);
-						
-						//update min max
-						for(int m = 0; m < 3; m++){
-							minMax[0][m] = Math.min(minMax[0][m], transformed[m]/transformed[3]);
-							minMax[1][m] = Math.max(minMax[0][m], transformed[m]/transformed[3]);
-						}
-						
-						x = outerVolume.getMaxX();
+		//test points for borders since they may overlap
+		float[][]minMax={{Float.MAX_VALUE,Float.MAX_VALUE,Float.MAX_VALUE},
+				{Float.MIN_VALUE,Float.MIN_VALUE,Float.MIN_VALUE}};
+		float z = outerVolume.getMinZ();
+		for(int i= 0; i < 2; i++ ){
+			float y = outerVolume.getMinY();
+			for(int j =0; j < 2; j++){
+				float x =  outerVolume.getMinX();
+				for(int k =0; k < 2; k++){
+					float transformer[]={x,y,z,1};
+					float transformed[] = new float[4];
+
+					sourceTransJogl.multVec(transformer,transformed);
+
+					//update min max
+					for(int m = 0; m < 3; m++){
+						minMax[0][m] = Math.min(minMax[0][m], transformed[m]/transformed[3]);
+						minMax[1][m] = Math.max(minMax[0][m], transformed[m]/transformed[3]);
 					}
-					y = outerVolume.getMaxY();
+
+					x = outerVolume.getMaxX();
 				}
-				z = outerVolume.getMaxZ();
+				y = outerVolume.getMaxY();
 			}
-			//add box
-			results[n] = new AABBox(minMax[0], minMax[1]);
+			z = outerVolume.getMaxZ();
 		}
-		return results;
+		return new AABBox(minMax[0], minMax[1]);
 	}
-	
 }
