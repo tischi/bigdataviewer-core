d0220b79d63fcfb30f8dbb76137d4fd43bfcbd46
Christian Tischer <christian.tischer@embl.de>
2020-02-06 11:44:30 +0100

    Change N5S3LoaderDemo

--- src/test/java/bdv/io/n5/N5S3LoaderDemo.java
+++ src/test/java/bdv/io/n5/N5S3LoaderDemo.java
@@ -10,7 +10,23 @@ public class N5S3LoaderDemo
 {
 	public static void main( String[] args ) throws SpimDataException
 	{
-		final File file = new File( "src/test/resources/platy-n5s3.xml" );
-		BigDataViewer.open( file.getAbsolutePath(), file.getName(), null, ViewerOptions.options() );
+		// works nice
+//		BigDataViewer.open( "https://git.embl.de/tischer/platy-browser-tables/raw/master/data/test_n5/1.0.0/images/remote/sbem-6dpf-1-whole-raw-1.xml", "", null, ViewerOptions.options() );
+
+		// even nicer (but only when in the right orientation)
+//		BigDataViewer.open( "https://git.embl.de/tischer/platy-browser-tables/raw/master/data/test_n5/1.0.0/images/remote/sbem-6dpf-1-whole-raw-2.xml", "", null, ViewerOptions.options() );
+
+		// feels slower than above
+//		BigDataViewer.open( "https://git.embl.de/tischer/platy-browser-tables/raw/master/data/test_n5/1.0.0/images/remote/sbem-6dpf-1-whole-raw-3.xml", "", null, ViewerOptions.options().numRenderingThreads( 20 ) );
+
+		// feels even slower
+//		BigDataViewer.open( "https://git.embl.de/tischer/platy-browser-tables/raw/master/data/test_n5/1.0.0/images/remote/sbem-6dpf-1-whole-raw-4.xml", "", null, ViewerOptions.options().numRenderingThreads( 3 ) );
+
+		// feels even slower
+		BigDataViewer.open( "https://git.embl.de/tischer/platy-browser-tables/raw/master/data/test_n5/1.0.0/images/remote/sbem-6dpf-1-whole-segmented-cells.xml", "", null, ViewerOptions.options().numRenderingThreads( 3 ) );
+
+
+//		BigDataViewer.open( "https://git.embl.de/tischer/platy-browser-tables/raw/master/data/test_n5/1.0.0/images/remote/sbem-6dpf-1-whole-raw.xml", "", null, ViewerOptions.options() );
+
 	}
 }
